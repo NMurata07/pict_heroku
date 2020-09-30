bf2977898020f3728f31eb2a9a5d2c47abc909e1
test <test@test>
2020-09-30 16:27:14 +0900

    add infinity loop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def index
+    begin
+      loop do
+        @u = User.new(name: "ああああああああ", email:"qwertyasdf@zxcvaddf.com", password:"aaaaaaaaaaaaaaa")
+        @u.save!
+      end
+    rescue => e
+      p e
+    end
   end
 end
